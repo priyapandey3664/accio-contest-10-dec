c093a58314079040b89edd59d9ed557c8d68f510
priya pandey <piapandey36@gmail.com>
2022-12-10 20:54:10 +0530

    final update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,34 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React from 'react'
+import { useEffect, useState } from 'react'
+
 
 const Home = () => {
-  const [data, setData] = useState([]);
+    const [data,setData] = useState([]);
 
-  useEffect(() => {
-    const API = fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
-    )
-      .then((res) => res.json())
-      // console.log(res);
-      .then((user) => setData(user));
-  }, []);
+    useEffect(()=> {
+        const API = fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+        .then((res)=> res.json())
+        // console.log(res);
+        .then((user)=>setData(user));
+    },[])
   return (
     <>
-      {data.map((e) => {
-        return (
-          <div className="main" style={{ display: "flex",justifyContent:"space-evenly"}} key={e.id}>
-            <div  style={{ display: "flex", alignItems:"center"}}>
-              <img src={e.image} alt="image" />
-              <h5 style={{ marginLeft:"5px"}}>{e.name}</h5>
-            </div>
-            <div>
-              <h4>{e.symbol}</h4>
-            </div>
-            <div>
-              <h3>{e.current_price}</h3>
-            </div>
-            <div>
-              <h3>{e.total_volume}</h3>
-            </div>
-            <div>
-              <h3>{e.price_change_24h}</h3>
-            </div>
-            <div>
-              <h3>{e.fully_diluted_valuation}</h3>
+    {data.map((e)=> {
+        return <div key={e.div}>
+            <img src={e.image}/>
+            <h2>{e.name}</h2>
             
-            </div>
-             
-          </div>
-        
-        );
-      })}
+           <div className='one'>
+           <p>{e.id}</p>
+            <h3 className='sym'>{e.symbol}</h3>
+            <h3 className='price'>{e.current_price}</h3>
+            <h3 className='vol'>{e.total_volume}</h3>
+           </div>
+            <hr></hr>
+        </div>
+    })}
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
